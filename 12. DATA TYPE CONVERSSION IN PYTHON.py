33221d4dbd072c5022f781aa86775b24215cd9ef
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-05 20:48:42 +0500

    first code

---
+++ 12. DATA TYPE CONVERSSION IN PYTHON.py
@@ -0,0 +1,26 @@
+
+bill = int(input(" Enter your amount: "))
+sale_tax = bill * 10 /100
+print(f"Sale Tax:  + {sale_tax}")
+total_bill = bill + sale_tax
+print(f"your total bill amount is {total_bill}")
+
+
+
+
+employee_name = input("Enter your name Plz: ")
+basic_pay = int(input("Your Basic Pay: "))
+bonus = 15000
+monthly_salary = basic_pay + bonus
+is_already_get_advance_pay = bool(input("Do you have already get advance pay? "))
+
+print(f"{employee_name} Monthly Salary {monthly_salary}")
+
+
+
+email = input("Enter your E-mail: ")
+passward = input("Password: ")
+reference_account = input("Reference Account: ")
+another_account = bool(input("Do you have another account? "))
+print(f"your Email {email} is active sucessfully now. {reference_account} as a reference is acceptable. Thak You for Visiting in our side.")
+
